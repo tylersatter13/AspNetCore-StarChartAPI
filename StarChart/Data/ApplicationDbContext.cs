57b308029be8635b109839720ecb21fe4a159748
tylersatter <tsman13@gmail.com>
2019-11-12 15:44:10 -0800

    Added Celestial Object

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using StarChart.Models;
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjectsl;
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id;
+
+        [Required]
+        public string Name;
+
+        public int? OrbitedObjectId;
+
+        [NotMapped]
+        public List<CelestialObject> Satellites;
+
+        public TimeSpan OrbitalPeriod;
+
+    }
+}
