9e346d884f57850e0c3d67fdf0d9008246fce0e2
tylersatter <tsman13@gmail.com>
2019-11-12 18:35:55 -0800

    Fixed Typo in database context

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<CelestialObject> CelestialObjectsl;
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
